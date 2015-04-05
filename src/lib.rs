e70b23513ecb0fdb04185333430700d30d8ce05f
Tim Ryan <id@timryan.org>
2015-04-05 09:45:57 -0400

    Adds initial classes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+use std::collections::HashMap;
+
+type DSpan<'a> = Vec<&'a DocElement<'a>>;
+
+enum DocElement<'a> {
+	DString(String),
+	DGroup(HashMap<String, String>, DSpan<'a>),
+}
+
 #[test]
-fn it_works() {
+fn try_this() {
+	fn it_works(val:DocElement) {
+		match val {
+			DocElement::DString(..) => {},
+			DocElement::DGroup(..) => {},
+		}
+	}
+
+	it_works(DocElement::DString("Hello world!".to_string()));
+	it_works(DocElement::DGroup(HashMap::new(), vec![]));
+}
+
+
+fn main() {
+	let val = DocElement::DString("Hello world!".to_string());
+	match val {
+		_ => {},
+	}
 }
