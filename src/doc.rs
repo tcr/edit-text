990aa067331ae4fcf2311ef27b85c5161b31072c
Tim Ryan <id@timryan.org>
2015-04-05 10:30:05 -0400

    Adds atoms and iteration.

--- src/doc.rs
+++ src/doc.rs
@@ -1,8 +1,25 @@
 use std::collections::HashMap;
 
-pub type DSpan = Vec<DocElement>;
+#[derive(PartialEq)]
+pub enum Atom {
+	Char(char),
+	Enter(HashMap<String, String>),
+	Leave,
+}
+
+pub type DocSpan = Vec<DocElement>;
 
 pub enum DocElement {
-	DString(String),
-	DGroup(HashMap<String, String>, DSpan),
+	DocChars(String),
+	DocGroup(HashMap<String, String>, DocSpan),
+}
+
+
+pub type DelSpan = Vec<DelElement>;
+
+pub enum DelElement {
+	DelChars(i32),
+	WithChars(i32),
+	DelGroup(DelSpan),
+	WithGroup(DelSpan),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
+#![feature(collections)]
+
 mod doc;
 
 use std::collections::HashMap;
-use doc::{DSpan, DocElement};
+use doc::{DocSpan, DocElement, Atom};
 use doc::DocElement::*;
 
-pub fn debug_span(val:&DSpan) {
+pub fn debug_span(val:&DocSpan) {
 	for i in val {
 		debug_elem(i);
 	}
@@ -12,29 +14,69 @@ pub fn debug_span(val:&DSpan) {
 
 pub fn debug_elem(val:&DocElement) {
 	match val {
-		&DString(ref value) => {
+		&DocChars(ref value) => {
 			println!("str({})", value);
 		},
-		&DGroup(ref attrs, ref span) => {
+		&DocGroup(ref attrs, ref span) => {
 			println!("attrs({})", attrs.capacity());
 			println!("span({})", span.capacity());
 		},
 	}
 }
 
-pub fn simple() -> DSpan {
+pub fn simple() -> DocSpan {
 	vec![
-		DString("Hello world!".to_string()),
-		DGroup(HashMap::new(), vec![]),
+		DocChars("Hello world!".to_string()),
+		DocGroup(HashMap::new(), vec![]),
 	]
+}
+
+pub fn iterate(span:&DocSpan) -> Vec<Atom> {
+	let mut atoms = vec![];
+	for elem in span {
+		match elem {
+			&DocChars(ref value) => {
+				for c in value.chars() {
+					atoms.push(Atom::Char(c));
+				}
+			},
+			&DocGroup(ref attrs, ref span) => {
+				atoms.push(Atom::Enter(attrs.clone()));
+				atoms.append(&mut iterate(span));
+				atoms.push(Atom::Leave);
+			},
+		}
+	}
+	atoms
 }
 
 #[test]
 fn try_this() {
-	let source:DSpan = vec![
-		DString("Hello world!".to_string()),
-		DGroup(HashMap::new(), vec![]),
+	let source:DocSpan = vec![
+		DocChars("Hello world!".to_string()),
+		DocGroup(HashMap::new(), vec![]),
 	];
 
 	debug_span(&source);
+
+	let source_atoms = vec![
+		Atom::Char('H'),
+		Atom::Char('e'),
+		Atom::Char('l'),
+		Atom::Char('l'),
+		Atom::Char('o'),
+		Atom::Char(' '),
+		Atom::Char('w'),
+		Atom::Char('o'),
+		Atom::Char('r'),
+		Atom::Char('l'),
+		Atom::Char('d'),
+		Atom::Char('!'),
+		Atom::Enter(HashMap::new()),
+		Atom::Leave,
+	];
+	
+	if !(iterate(&source) == source_atoms) {
+		panic!("iteration doesnt match");
+	}
 }
