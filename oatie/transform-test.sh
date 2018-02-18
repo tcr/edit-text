2974e34376e99dd523112e7f9de6ad7f4dec2630
Tim Ryan <id@timryan.org>
2018-02-18 13:12:52 -0500

    Removes custom parser for ron parser.

--- oatie/transform-test.sh
+++ oatie/transform-test.sh
@@ -6,7 +6,8 @@ cargo build --release
 set +e
 
 for file in in/*; do
-    # do something on $file
+    test -f "${file}" || continue
+    echo
     echo $file
     cat "$file" | RUST_BACKTRACE=1 ../target/release/oatie-transform > /dev/null
 done
