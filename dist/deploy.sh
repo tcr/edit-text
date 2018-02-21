fd11b44f6073fa6422d09bc8bd5be419d918ec82
Tim Ryan <id@timryan.org>
2018-02-20 23:06:48 -0500

    Adds shift+enter for return key.

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -3,9 +3,16 @@
 set -e
 set -x
 
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
 cd ..
+
+make js-build; make wasm
+
 vagrant up
 vagrant ssh -c "cd /vagrant/mercutio; cargo build --release --target=x86_64-unknown-linux-gnu --bin mercutio-sync"
+
 cd dist
 
 docker build -t dokku/edit-text:latest .; docker save dokku/edit-text:latest | bzip2 | ssh root@sandbox.edit.io "bunzip2 | docker load"; ssh root@sandbox.edit.io "dokku tags:deploy edit-text latest"
