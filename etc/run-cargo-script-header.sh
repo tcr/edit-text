e44d54357a886e1ccb80b94199f3e32d8026105a
Tim Ryan <id@timryan.org>
2018-03-07 01:46:06 -0500

    Updates x.rs friendly script.

--- etc/run-cargo-script-header.sh
+++ etc/run-cargo-script-header.sh
@@ -5,12 +5,14 @@
 
 GREY='\033[1;30m'
 NC='\033[0m' # No Color
-HEADER="${GREY}Building cargo script...${NC} \r\n"
+HEADER="${GREY}Building cargo script... (this will be cached afterward)${NC} \r\n"
+
+temp_file=$(mktemp)
 
 cargo script --debug --build-only $1 2>&1 |
-    perl -pe "s/^(.)/$HEADER\1/g if $. == 1; print STDERR \"$.\n\"" 2>/tmp/run_status
+    perl -pe "s/^(.)/$HEADER\1/g if $. == 1; print STDERR \"$.\n\"" 2>$temp_file
 BUILD_STATUS=${PIPESTATUS[0]}
-HEADER_STATUS=$(cat /tmp/run_status | wc -l)
+HEADER_STATUS=$(cat $temp_file | wc -l)
 
 if [[ $BUILD_STATUS != 0 ]]; then
     exit $BUILD_STATUS
