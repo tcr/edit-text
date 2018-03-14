38e17e612cae05798125511578ebbd91d8e1e3c1
Tim Ryan <id@timryan.org>
2018-03-13 23:35:44 -0400

    Commit to Unicode A S A P

--- mercutio-server/src/sync.rs
+++ mercutio-server/src/sync.rs
@@ -50,17 +50,15 @@ pub fn default_doc() -> Doc {
 
 This is edit-text, a web-based rich text editor.
 
-* Version 0.1. This is a very early preview. :)
+* This is a very early preview. 👼
 
 * Supports collaborative editing.
 
-* Written in Rust and cross-compiled to WebAssembly.
+* Written in Rust in the backend, cross-compiled to WebAssembly on the frontend.
 
 * Supports Markdown export.
 
-This app is very easy to break! That's okay though. We'll notice and fix it, and it'll break less in the future.
-
----
+This app might be easy to break! That's okay though. We'll notice and fix it, and it'll break less in the future.
 
 Type github.com/tcr/edit-text into your search bar for more information.
 
