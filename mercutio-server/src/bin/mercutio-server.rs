6ddb5432028b1f2b22f712eb27e7221170d97574
Tim Ryan <id@timryan.org>
2018-03-19 22:55:28 -0400

    Switches to rouille as a static file server.

--- mercutio-server/src/bin/mercutio-server.rs
+++ mercutio-server/src/bin/mercutio-server.rs
@@ -11,6 +11,8 @@ extern crate mercutio;
 extern crate mercutio_server;
 extern crate oatie;
 extern crate rand;
+#[macro_use]
+extern crate rouille;
 extern crate serde;
 extern crate serde_json;
 extern crate structopt;
@@ -28,14 +30,12 @@ use std::collections::HashMap;
 use std::panic;
 use std::path::{Path, PathBuf};
 use std::process;
-use std::sync::Arc;
 use std::thread;
 use std::fs::File;
 use std::io::prelude::*;
+use rouille::Response;
 use std::thread::JoinHandle;
 use structopt::StructOpt;
-use tiny_http::{Header, Response};
-use url::Url;
 use mime_guess::guess_mime_type;
 
 trait Dir: Sync + Send {
@@ -82,134 +82,7 @@ impl Dir for LocalDir {
     }
 }
 
-
-fn spawn_server_thread(
-    server: Arc<tiny_http::Server>,
-    client_proxy: bool,
-    dist_dir: Box<Dir>,
-    template_dir: Box<Dir>,
-) -> JoinHandle<()> {
-    thread::spawn(move || {
-        loop {
-            let req = server.recv().unwrap();
-
-            // Extract just the path segment from this URL.
-            // The `url` crate needs an absolute base to create a Url.
-            let path = Url::parse("http://localhost/")
-                .unwrap()
-                .join(req.url())
-                .unwrap()
-                .path()
-                .to_owned();
-
-            let update_config_var = |data: &[u8]| -> Vec<u8> {
-                let input = String::from_utf8_lossy(data);
-                let output = input.replace(
-                    "CONFIG = {}",
-                    &format!(
-                        "CONFIG = {{configured: true, wasm: {}}}",
-                        if client_proxy { "false" } else { "true" }
-                    ),
-                );
-                output.into_bytes()
-            };
-
-            match path.as_ref() {
-                "/" | "/index.html" => {
-                    // Redirect as random client
-                    let mut rng = thread_rng();
-                    let new_page_id = ::rand::seq::sample_iter(&mut rng, 0..26u8, 8)
-                        .unwrap()
-                        .into_iter()
-                        .map(|x| (b'a' + x) as char)
-                        .collect::<String>();
-                    let mut res = Response::empty(302);
-                    let mut h = Header::from_bytes(
-                        b"Location".to_vec(),
-                        format!("/{}#helloworld", new_page_id).as_bytes(),
-                    ).unwrap();
-                    res.add_header(h);
-                    let _ = req.respond(res);
-                }
-
-                "/favicon.png" => {
-                    let data = template_dir.get(Path::new("favicon.png")).unwrap();
-                    let _ = req.respond(
-                        Response::from_data(data).with_header(
-                            Header::from_bytes("content-type".as_bytes(), "image/png".as_bytes())
-                                .unwrap(),
-                        ),
-                    );
-                }
-
-                "/$/multi" | "/$/multi/" => {
-                    let data = template_dir.get(Path::new("multi.html")).unwrap();
-                    let _ = req.respond(
-                        Response::from_data(update_config_var(&data)).with_header(
-                            Header::from_bytes("content-type".as_bytes(), "text/html".as_bytes())
-                                .unwrap(),
-                        ),
-                    );
-                }
-
-                path => {
-                    // Skip the initial "/$/"
-                    if path.starts_with("/$/") {
-                        let path = path.chars().skip(3).collect::<String>();
-                        if let Some(target) = dist_dir.get(Path::new(&path)) {
-                            let _ = req.respond(
-                                Response::from_data(target).with_header(
-                                    Header::from_bytes(
-                                        "content-type".as_bytes(),
-                                        guess_mime_type(&path).to_string().as_bytes(),
-                                    ).unwrap(),
-                                ),
-                            );
-                        } else {
-                            let _ = req.respond(Response::from_string("404".to_owned()));
-                        }
-                    } else {
-                        // Possibly a page?
-                        let path_segs = (path[1..]).split('/').collect::<Vec<_>>();
-                        if valid_page_id(&path_segs[0]) && path_segs.len() == 2
-                            && path_segs[1] == "presentation"
-                        {
-                            let data = template_dir.get(Path::new("presentation.html")).unwrap();
-                            let _ = req.respond(
-                                Response::from_data(update_config_var(&data)).with_header(
-                                    Header::from_bytes(
-                                        "content-type".as_bytes(),
-                                        "text/html".as_bytes(),
-                                    ).unwrap(),
-                                ),
-                            );
-                        } else if valid_page_id(&path_segs[0]) && path_segs.len() == 1 {
-                            let data = template_dir.get(Path::new("client.html")).unwrap();
-                            let _ = req.respond(
-                                Response::from_data(update_config_var(&data)).with_header(
-                                    Header::from_bytes(
-                                        "content-type".as_bytes(),
-                                        "text/html".as_bytes(),
-                                    ).unwrap(),
-                                ),
-                            );
-                        } else {
-                            // TODO real 404 error code
-                            let _ = req.respond(Response::from_string("404".to_owned()));
-                        }
-                    }
-                }
-            }
-        }
-    })
-}
-
 fn run_http_server(port: u16, client_proxy: bool) {
-    let server = tiny_http::Server::http(&format!("0.0.0.0:{}", port)).unwrap();
-
-    let server = Arc::new(server);
-    let mut guards = Vec::with_capacity(4);
-
     let dist_dir: Box<Dir>;
     let template_dir: Box<Dir>;
     if cfg!(feature = "standalone") {
@@ -226,24 +99,103 @@ fn run_http_server(port: u16, client_proxy: bool) {
     assert!(template_dir.exists(Path::new("presentation.html")));
     assert!(template_dir.exists(Path::new("favicon.png")));
 
-    for _ in 0..4 {
-        let server = server.clone();
-
-        let guard = spawn_server_thread(
-            server.clone(),
-            client_proxy,
-            dist_dir.clone(),
-            template_dir.clone(),
-        );
-
-        guards.push(guard);
-    }
-
     println!("Listening on http://localhost:{}/", port);
 
-    for guard in guards {
-        let _ = guard.join();
-    }
+    #[allow(unused)]
+    #[allow(unreachable_code)]
+    rouille::start_server(format!("0.0.0.0:{}", port), move |request| {
+        let update_config_var = |data: &[u8]| -> Vec<u8> {
+            let input = String::from_utf8_lossy(data);
+            let output = input.replace(
+                "CONFIG = {}",
+                &format!(
+                    "CONFIG = {{configured: true, wasm: {}}}",
+                    if client_proxy { "false" } else { "true" }
+                ),
+            );
+            output.into_bytes()
+        };
+
+        fn random_id() -> String {
+            let mut rng = thread_rng();
+            return ::rand::seq::sample_iter(&mut rng, 0..26u8, 8)
+                .unwrap()
+                .into_iter()
+                .map(|x| (b'a' + x) as char)
+                .collect::<String>();
+        }
+
+        router!(request,
+
+            // Redirect root page to random page ID
+            (GET) ["/"] => {
+                return Response::redirect_302(
+                    format!("/{}#helloworld", random_id()),
+                );
+            },
+            (GET) ["/index.html"] => {
+                return Response::redirect_302(
+                    format!("/{}#helloworld", random_id()),
+                );
+            },
+
+            (GET) ["/favicon.png"] => {
+                return Response::from_data(
+                    "image/png",
+                    template_dir.get(Path::new("favicon.png")).unwrap(),
+                );
+            },
+
+            (GET) ["/$/multi"] => {
+                return Response::from_data(
+                    "text/html",
+                    update_config_var(
+                        &template_dir.get(Path::new("multi.html")).unwrap(),
+                    ),
+                );
+            },
+            (GET) ["/$/multi/"] => {
+                return Response::redirect_302("/$/multi");
+            },
+
+            (GET) ["/$/{target}", target: String] => {
+                if let Some(data) = dist_dir.get(Path::new(&target)) {
+                    return Response::from_data(
+                        guess_mime_type(&target).to_string(),
+                        data,
+                    );
+                } else {
+                    return Response::empty_404();
+                }
+            },
+
+            (GET) ["/{id}/presentation", id: String] => {
+                return Response::from_data(
+                    "text/html",
+                    update_config_var(
+                        &template_dir.get(Path::new("presentation.html")).unwrap(),
+                    ),
+                );
+            },
+            (GET) ["/{id}/presentation/", id: String] => {
+                return Response::redirect_302(format!("/{}/presentation", id));
+            },
+
+            (GET) ["/{id}", id: String] => {
+                return Response::from_data(
+                    "text/html",
+                    update_config_var(
+                        &template_dir.get(Path::new("client.html")).unwrap(),
+                    ),
+                );
+            },
+            (GET) ["/{id}/", id: String] => {
+                return Response::redirect_302(format!("/{}", id));
+            },
+
+            _ => Response::empty_404()
+        )
+    });
 }
 
 #[derive(StructOpt, Debug)]
