7428a7edad4841603617dd0de8c0f1009a4c6a2e
Tim Ryan <id@timryan.org>
2018-10-29 01:25:44 -0400

    Incorporates better checkpoint features.

--- build-tools/tests/integration.rs
+++ build-tools/tests/integration.rs
@@ -18,17 +18,15 @@ use self::common::*;
 fn integration_spooky_test() {
     concurrent_editing(async move |mut debug, test_id, _checkpoint| {
         // Position the caret at the end of the current line.
-        await!(sleep_ms(1_000));
         await!(debug.debug_end_of_line());
+        await!(sleep_ms(1_000));
 
         // Type a ghost emoji.
-        await!(sleep_ms(1_000));
+        // Wait an arbitrary 4s for clients to receive all pending operations.
         await!(debug.keypress("0x1f47b"));
-
-        // Wait 4s for clients to receive all pending operations.
         await!(sleep_ms(4_000));
 
-        // Get the innerText of the first h1.
+        // Get the Markdown content.
         let markdown = await!(debug.as_markdown())?;
         eprintln!("[{}] markdown content: {:?}", test_id, markdown);
 
@@ -36,3 +34,34 @@ fn integration_spooky_test() {
         Ok(markdown.lines().next().unwrap().ends_with("👻👻"))
     });
 }
+
+/// Clients typing in a sequential order should have predictable results.
+#[test]
+fn integration_sequential_test() {
+    concurrent_editing(async move |mut debug, test_id, mut checkpoint| {
+        // Type a character.
+        checkpoint.sequential();
+        await!(debug.keypress(&format!(r#""{}".charCodeAt(0)"#, checkpoint.index)));
+        checkpoint.sync();
+
+        // Type a character.
+        checkpoint.sequential();
+        await!(debug.keypress(&format!(r#""{}".charCodeAt(0)"#, checkpoint.index)));
+        checkpoint.sync();
+
+        // Type a character.
+        checkpoint.sequential();
+        await!(debug.keypress(&format!(r#""{}".charCodeAt(0)"#, checkpoint.index)));
+        checkpoint.sync();
+
+        // Wait an arbitrary 4s for clients to receive all pending operations.
+        await!(sleep_ms(4_000));
+
+        // Get the Markdown content.
+        let markdown = await!(debug.as_markdown())?;
+        eprintln!("[{}] markdown content: {:?}", test_id, markdown);
+
+        Ok(markdown.find("000").is_some() && markdown.find("111").is_some())
+    });
+}
+
