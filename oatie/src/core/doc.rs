55cc98ab6b71008147205ab6363ac04f962913c1
Tim Ryan <id@timryan.org>
2018-12-31 02:51:23 -0500

    Moves all content out of lib.rs in oatie.

--- oatie/src/core/doc.rs
+++ oatie/src/core/doc.rs
@@ -4,6 +4,10 @@ use serde::{
     Deserialize,
     Serialize,
 };
+use crate::ot::*;
+use crate::apply::*;
+use crate::compose::*;
+use crate::transform::transform;
 
 // Re-exports
 pub use super::place::*;
@@ -17,6 +21,47 @@ pub type CurSpan = Vec<CurElement>;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Op<S: Schema>(pub DelSpan<S>, pub AddSpan<S>);
+
+impl<S: Schema> OT<S> for Op<S> {
+    type Doc = Doc<S>;
+
+    fn apply(doc: &Self::Doc, op: &Self) -> Self::Doc {
+        Doc(apply_operation(&doc.0, op))
+    }
+
+    fn empty() -> Self {
+        Op(vec![], vec![])
+    }
+
+    fn compose(a: &Self, b: &Self) -> Self {
+        compose(a, b)
+    }
+
+    fn compose_iter<'a, I>(iter: I) -> Self
+    where
+        I: Iterator<Item = &'a Self>,
+        S: 'a,
+    {
+        let mut base = Self::empty();
+        for item in iter {
+            base = Self::compose(&base, item);
+        }
+        base
+    }
+
+    fn transform(a: &Self, b: &Self) -> (Self, Self) {
+        transform::<S>(&a, &b)
+    }
+
+    fn transform_advance(a: &Self, b: &Self) -> Self {
+        let (a_transform, _b_transform) = Self::transform(a, b);
+        let a_res = Self::compose(a, &a_transform);
+        // let b_res = Self::compose(b, &b_transform);
+        // assert_eq!(a_res, b_res);
+        a_res
+    }
+}
+
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum DocElement<S: Schema> {
---
+++ oatie/src/ot.rs
@@ -0,0 +1,36 @@
+use crate::schema::*;
+
+/// A type that can have operational transform applied to it.
+/// The `OT` trait is implemented on an operation object, and its
+/// associated type `Doc` is what the operation should operate on.
+pub trait OT<S: Schema>
+where
+    Self: Sized,
+{
+    type Doc;
+
+    /// Applies an operation to a `Self::Doc`, returning the modified `Self::Doc`.
+    fn apply(doc: &Self::Doc, op: &Self) -> Self::Doc;
+
+    /// Returns an empty operation.
+    fn empty() -> Self;
+
+    /// Composes two operations, returning a single operation encapsulating them
+    /// both.
+    fn compose(a: &Self, b: &Self) -> Self;
+
+    /// Composes an iterator of operations into a single operation.
+    /// If no operations are returned from the iterator, the Op::empty() should be
+    /// returned.
+    fn compose_iter<'a, I>(iter: I) -> Self
+    where
+        I: Iterator<Item = &'a Self>,
+        Self: 'a;
+
+    /// Transform a document given the corresponding Schema trait.
+    fn transform(a: &Self, b: &Self) -> (Self, Self);
+
+    /// Utility function to transform an operation against a competing one,
+    /// returning the results of composing them both.
+    fn transform_advance(a: &Self, b: &Self) -> Self;
+}
