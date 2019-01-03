b6c79d63df628b0ec9bd8cb3e0b6a602f2da4bf4
Tim Ryan <id@timryan.org>
2019-01-03 01:09:36 -0500

    Disable the source code poller.

--- edit-frontend/src/ui/app.tsx
+++ edit-frontend/src/ui/app.tsx
@@ -282,37 +282,39 @@ export class EditorFrame extends React.Component {
       });
     };
 
-    // Update watcher.
-    // TODO move this to a better location.
-    let cachedEtag: null | string = null;
-    let refreshNotification = false;
-    let intervalId = setInterval(() => {
-      fetch(new Request('/$/edit.js'), {
-        method: 'GET',
-        mode: 'same-origin',
-        cache: 'default',
-      }).then(res => {
-        if (res.ok) {
-          let etag = res.headers.get('etag');
-          if (etag) {
-            if (!cachedEtag) {
-              cachedEtag = etag;
-            } else {
-              if (cachedEtag != etag) {
-                if (!refreshNotification) {
-                  refreshNotification = true;
-                  this.showNotification({
-                    level: 'notice',
-                    element: <div>A newer version of edit-text is available. <button onClick={() => window.location.reload()}>Refresh the page</button></div>,
-                  });
-                }
-                clearInterval(intervalId);
-              }
-            }
-          }
-        }
-      });
-    }, 3000);
+    // Update source code watcher to be notified of a newer browser version.
+    // TODO This should really just be webpack's auto-reload functionality or
+    // something equivalent. This version causes the file to just be reloaded
+    // every two minutes. Impractical.
+    // let cachedEtag: null | string = null;
+    // let refreshNotification = false;
+    // let intervalId = setInterval(() => {
+    //   fetch(new Request('/$/edit.js'), {
+    //     method: 'GET',
+    //     mode: 'same-origin',
+    //     cache: 'default',
+    //   }).then(res => {
+    //     if (res.ok) {
+    //       let etag = res.headers.get('etag');
+    //       if (etag) {
+    //         if (!cachedEtag) {
+    //           cachedEtag = etag;
+    //         } else {
+    //           if (cachedEtag != etag) {
+    //             if (!refreshNotification) {
+    //               refreshNotification = true;
+    //               this.showNotification({
+    //                 level: 'notice',
+    //                 element: <div>A newer version of edit-text is available. <button onClick={() => window.location.reload()}>Refresh the page</button></div>,
+    //               });
+    //             }
+    //             clearInterval(intervalId);
+    //           }
+    //         }
+    //       }
+    //     }
+    //   });
+    // }, 3000);
 
     this.state = {
       body: this.props.body,
